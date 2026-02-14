e830a7e277f803edbd49b48bfdbc75dce67a7ebc
manikantarguktn <manikantarguktn@gmail.com>
2026-02-14 19:19:14 +0530

    updated the tests

--- tests/launch5.spec.ts
+++ tests/launch5.spec.ts
@@ -1,4 +1,4 @@
 import { test, expect } from '@playwright/test';
-test.only('@sanity Run this test only 5', async ({ page }) => {
+test('@sanity Run this test only 5', async ({ page }) => {
   await page.goto('https://example.com');
 });
