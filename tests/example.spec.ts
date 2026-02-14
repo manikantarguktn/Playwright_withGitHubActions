22295db7d5b026a9ae84715333fcf826c22e8bbe
manikantarguktn <manikantarguktn@gmail.com>
2026-02-14 19:23:21 +0530

    updated the sanity

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('@sanity has title', async ({ page }) => {
+test(' has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('@sanity get started link', async ({ page }) => {
+test(' get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
--- tests/launch2.spec.ts
+++ tests/launch2.spec.ts
@@ -1,4 +1,4 @@
 import { test, expect } from '@playwright/test';
-test('@sanityRun this test only 2', async ({ page }) => {
+test('@sanity Run this test only 2', async ({ page }) => {
   await page.goto('https://example.com');
 });
